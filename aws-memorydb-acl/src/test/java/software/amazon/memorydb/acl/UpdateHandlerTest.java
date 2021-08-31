31acd0f03a84123752ba149672d1f9f2f899d55a
Aditya Rao <adityagnrao@gmail.com>
2021-08-31 15:07:59 -0700

    remove referrence to junit.framework

--- aws-memorydb-acl/src/test/java/software/amazon/memorydb/acl/UpdateHandlerTest.java
+++ aws-memorydb-acl/src/test/java/software/amazon/memorydb/acl/UpdateHandlerTest.java
@@ -32,9 +32,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.fail;
+import static org.assertj.core.api.Assertions.fail;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
@@ -243,8 +241,8 @@ public class UpdateHandlerTest extends AbstractTestBase {
 
         verify(sdkClient).updateACL(updateAclCapture.capture());
         UpdateAclRequest convertedRequest = updateAclCapture.getValue();
-        assertEquals(convertedRequest.userNamesToAdd(), userIdsToAdd);
-        assertEquals(convertedRequest.userNamesToRemove(), userIdsToRemove);
+        assertThat(userIdsToAdd.equals(convertedRequest.userNamesToAdd()));
+        assertThat(userIdsToRemove.equals(convertedRequest.userNamesToRemove()));
     }
 
 }
