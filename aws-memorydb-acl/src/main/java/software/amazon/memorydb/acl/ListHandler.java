f8e63cd5f992127de82c03c023da7092e54de31f
Aditya Rao <adityagnrao@gmail.com>
2021-09-01 15:28:42 -0700

    [aws-memorydb-acl] make the ListHandler extend BaseHandlerStd

--- aws-memorydb-acl/src/main/java/software/amazon/memorydb/acl/ListHandler.java
+++ aws-memorydb-acl/src/main/java/software/amazon/memorydb/acl/ListHandler.java
@@ -7,32 +7,26 @@ import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
-public class ListHandler extends BaseHandler<CallbackContext> {
-
-    private AmazonWebServicesClientProxy proxy;
-    private MemoryDbClient client;
+public class ListHandler extends BaseHandlerStd {
 
     @Override
-    public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-        final AmazonWebServicesClientProxy proxy,
-        final ResourceHandlerRequest<ResourceModel> request,
-        final CallbackContext callbackContext,
-        final Logger logger) {
-
-        this.proxy = proxy;
-        this.client = ClientBuilder.getClient();
-
+    protected ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+            final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final CallbackContext callbackContext,
+            final ProxyClient<MemoryDbClient> proxyClient, Logger logger) {
         final DescribeAcLsRequest awsRequest = Translator.translateToListRequest(request.getNextToken());
 
         final DescribeAcLsResponse describeAclResponse = proxy.injectCredentialsAndInvokeV2(awsRequest,
-            this.client::describeACLs);
+                proxyClient.client()::describeACLs);
 
         return ProgressEvent.<ResourceModel, CallbackContext>builder()
-            .resourceModels(Translator.translateFromListRequest(describeAclResponse))
-            .nextToken(describeAclResponse.nextToken())
-            .status(OperationStatus.SUCCESS)
-            .build();
+                .resourceModels(Translator.translateFromListRequest(describeAclResponse))
+                .nextToken(describeAclResponse.nextToken())
+                .status(OperationStatus.SUCCESS)
+                .build();
     }
 }
